0c12c548868bc7926f41e21c7bc6f3704df6cd0c
conankzhang <conankzhang@gmail.com>
2022-07-18 01:02:28 -0700

    feat(Scene): Add random world generation

--- Source/main.cpp
+++ Source/main.cpp
@@ -59,41 +59,79 @@ Float3 getRayColorFromWorld(const Ray& ray, const TraceableList& world, int dept
     return (1.0f - rayEnd) * Float3(1.0f, 1.0f, 1.0f) + (Float3(0.5f, 0.7f, 1.0f) * rayEnd);
 }
 
+TraceableList GenerateWorld()
+{
+    TraceableList world;
+
+    const auto groundMaterial = std::make_shared<Lambertian>(Float3(0.5f, 0.5f, 0.5f));
+    world.Add(std::make_shared<Sphere>(Float3(0.0f, -1000.0f, 0.0f), 1000.0f, groundMaterial));
+
+    const auto glassMaterial = std::make_shared<Dielectric>(1.5f);
+    world.Add(std::make_shared<Sphere>(Float3(0.0f, 1.0f, 0.0f), 1.0f, glassMaterial));
+
+    const auto centerMaterial = std::make_shared<Lambertian>(Float3(0.4f, 0.2f, 0.1f));
+    world.Add(std::make_shared<Sphere>(Float3(-4.0f, 1.0f, 0.0f), 1.0f, centerMaterial));
+
+    const auto rightMaterial = std::make_shared<Metal>(Float3(0.7f, 0.6f, 0.5f), 0.0f);
+    world.Add(std::make_shared<Sphere>(Float3(4.0f, 1.0f, 0.0f), 1.0f, rightMaterial));
+
+    const Float3 center(4.0f, 0.2f, 0.0f);
+
+    for (int i = -11; i < 11; ++i)
+    {
+        for (int j = -11; j < 11; ++j)
+        {
+            const Float3 randomCenter(i + 0.9f * Math::RandomFloat(), 0.2f, j + 0.9f * Math::RandomFloat());
+            const float distanceFromCenter = (randomCenter - center).Length();
+            if (distanceFromCenter > 0.9f)
+            {
+                const float randomMaterial = Math::RandomFloat();
+                if (randomMaterial < 0.8f)
+                {
+                    const Float3 randomColor = Math::RandomFloat3(0.0f, 1.0f) * Math::RandomFloat3(0.0f, 1.0f);
+                    const auto lambertianMaterial = std::make_shared<Lambertian>(randomColor);
+                    world.Add(std::make_shared<Sphere>(randomCenter, 0.2f, lambertianMaterial));
+                }
+                else if (randomMaterial < 0.95f)
+                {
+                    const Float3 randomColor = Math::RandomFloat3(0.5f, 1.0f);
+                    const float randomFuzziness = Math::RandomFloat(0.0f, 0.5f);
+
+                    const auto metalMaterial = std::make_shared<Metal>(randomColor, randomFuzziness);
+                    world.Add(std::make_shared<Sphere>(randomCenter, 0.2f, metalMaterial));
+                }
+                else
+                {
+                    world.Add(std::make_shared<Sphere>(randomCenter, 0.2f, glassMaterial));
+                }
+            }
+        }
+    }
+
+    return world;
+}
+
 int main()
 {
     // Camera
-    constexpr float aspectRatio = 16.0f / 9.0f;
+    constexpr float aspectRatio = 3.0f / 2.0f;
     constexpr float verticalFov = 20.0f;
-    constexpr float aperture = 2.0f;
-    const Float3 cameraOrigin = Float3(3.0f, 3.0f, 2.0f);
-    const Float3 focalPoint = Float3(0.0f, 0.0f, -1.0f);
+    constexpr float aperture = 0.1f;
+    const Float3 cameraOrigin = Float3(13.0f, 2.0f, 3.0f);
+    const Float3 focalPoint = Float3(0.0f, 0.0f, 0.0f);
     const Float3 cameraUp = Float3(0.0f, 1.0f, 0.0f);
-    const float focalDistance = (cameraOrigin - focalPoint).Length();
+    const float focalDistance = 10.0f;
     const Camera camera(cameraOrigin, focalPoint, cameraUp, verticalFov, aspectRatio, aperture, focalDistance);
 
     // Image
-    constexpr int imageWidth = 400;
+    constexpr int imageWidth = 1200;
     constexpr int imageHeight = static_cast<int>(imageWidth / aspectRatio);
     constexpr int lastRowPixel = imageWidth - 1;
     constexpr int lastColPixel = imageHeight - 1;
-    constexpr int samplesPerPixel = 100;
+    constexpr int samplesPerPixel = 500;
     constexpr int maxBounceDepth = 50;
 
-    // World
-    TraceableList world;
-
-    const auto groundMaterial = std::make_shared<Lambertian>(Float3(0.8f, 0.8f, 0.0f));
-    world.Add(std::make_shared<Sphere>(Float3(0.0f, -100.5f, -1.0f), 100.0f, groundMaterial));
-
-    const auto centerMaterial = std::make_shared<Lambertian>(Float3(0.1f, 0.2f, 0.5f));
-    world.Add(std::make_shared<Sphere>(Float3(0.0f, 0.0f, -1.0f), 0.5f, centerMaterial));
-
-    const auto leftMaterial = std::make_shared<Dielectric>(1.5f);
-    world.Add(std::make_shared<Sphere>(Float3(-1.0f, 0.0f, -1.0f), 0.5f, leftMaterial));
-    world.Add(std::make_shared<Sphere>(Float3(-1.0f, 0.0f, -1.0f), -0.45f, leftMaterial));
-
-    const auto rightMaterial = std::make_shared<Metal>(Float3(0.8f, 0.6f, 0.2f), 0.0f);
-    world.Add(std::make_shared<Sphere>(Float3(1.0f, 0.0f, -1.0f), 0.5f, rightMaterial));
+    TraceableList world = GenerateWorld();
 
     // Render
     std::cout
